1adef56232458d42337ee797d8aadffddf39192f
Steve Kinney <hello@stevekinney.net>
2012-11-27 09:45:46 -0500

    Attempt to fix sorting

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() 
     { 
-        $("table#appointment-list").tablesorter();
+        $("table#appointment-list").tablesorter( {sortList: [[0,0], [1,0], [2,0]]}  );
     } 
 );
