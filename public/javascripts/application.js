1d2ae0875cb29f7fadd825e3da3180ab69f7d6b6
Steve Kinney <hello@stevekinney.net>
2012-11-29 10:20:12 -0500

    Add tablesorter information for appointments list

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,6 @@
 $(document).ready(function() 
     { 
         $("table#appointment-list").tablesorter( {sortList: [[0,0], [1,0], [2,0]]}  );
+        $("table#scheduled-appointments").tablesorter( {sortList: [[0,0], [1,0], [4,0]]}  );
     } 
 );
